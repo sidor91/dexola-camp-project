fc08b42972907de470c042754d8a15e5e3cbcf7d
Serhii Sydorenko <sidorchik91@gmail.com>
2023-08-29 17:35:36 +0300

    UPD | TOPNFT images

--- src/assets/Pics/TopNFTPics/topNFTpics.js
+++ src/assets/Pics/TopNFTPics/topNFTpics.js
@@ -1,13 +1,13 @@
-import pic1 from "./pic1.png";
-import pic1_2x from "./pic1_2x.png";
-import pic2 from "./pic2.png";
-import pic2_2x from "./pic2_2x.png";
-import pic3 from "./pic3.png";
-import pic3_2x from "./pic3_2x.png";
-import pic4 from "./pic4.png";
-import pic4_2x from "./pic4_2x.png";
-import pic5 from "./pic5.png";
-import pic5_2x from "./pic5_2x.png";
+import pic1 from "./pic1.webp";
+import pic1_2x from "./pic1_2x.webp";
+import pic2 from "./pic2.webp";
+import pic2_2x from "./pic2_2x.webp";
+import pic3 from "./pic3.webp";
+import pic3_2x from "./pic3_2x.webp";
+import pic4 from "./pic4.webp";
+import pic4_2x from "./pic4_2x.webp";
+import pic5 from "./pic5.webp";
+import pic5_2x from "./pic5_2x.webp";
 
 
 const nonRetinaPics = [pic1, pic2, pic3, pic4, pic5];
